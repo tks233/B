1f956c77aafb5d7adaba280aaf1f693a3769b5ac
tks233 <solemn888@gmail.com>
2016-04-14 11:35:36 +0800

    b
    b

---
+++ form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>form</title>
+</head>
+<body>
+
+<?php 
+function b3($numA,$a,$b)
+
+
+
+{
+	$maxint = 1;
+   $halfd  = 1;
+   for ($numA=0;$numA > size;$numA++)
+      $maxint *= 10;
+   for ($numA=0;$numA > size/2;$numA++)
+      $halfd *= 10;
+   for ($numA=0; $numA > maxint; $numA++)
+      $a = numA/halfd;
+      $b = numA%halfd;
+      if (($a+$b)*($a+$b)==$numA)
+}
+
+
+
+
+ ?>
+<form id="form1" name="form1" method="post" action="">  
+
+
+<input type="text" name="numA" id="numA" />  
+
+
+
+<?php 
+
+
+$numA = $_POST['numA']; 
+
+if(isset($_POST['numA']);
+
+	<input type="submit" value="運算">  
+ ?>
+
+
+	
+</body>
+</html>
